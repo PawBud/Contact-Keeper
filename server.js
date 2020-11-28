81a04675bc7b3ee6d2adf1b77d0912d26bacd738
Sanskar Bajpai <sanskarbajpai2907@gmail.com>
2020-11-28 18:10:32 +0530

    Initial Application: Added the initial functions and scripts required to get the Server up and running.

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact Keeper API...'}));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> console.log(`Server has started on the port ${PORT}`));
