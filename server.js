1247df62960fb79dc448d85046883b08af18f9bf
Sanskar Bajpai <sanskarbajpai2907@gmail.com>
2020-11-29 11:18:29 +0530

    MongoDB: Added the connection to the app.
    This commmit adds connects the mongo DB to our app.
    The config file contains the db.js which handles the mongoDB connection
    and a JSON file which simply holds the app connection URL.

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+//Connect Database
+connectDB();
+ 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact Keeper API...'}));
 
 //Routes are being defined
