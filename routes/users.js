46f79a5e73cf506a11bb5715f2e15bf839e01221
Sanskar Bajpai <sanskarbajpai2907@gmail.com>
2020-11-30 13:43:36 +0530

    UsersRoute: passwords are encypted and hashed.
    This commit inroduces password encryption and hashing. Moreover,
    it modifies the register route.

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -14,13 +15,39 @@ router.post('/', [
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Please enter a password with 8 or more characters').isLength({ min: 8 })
     ], 
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req)
         if(!errors.isEmpty()){
             return res.status(400).json({ errors: errors.array() });
         }
 
-        res.send('passed');
+        const { name, email, password } = req.body;
+        
+        try {
+            let user = await User.findOne({ email });
+
+            if(user){//if user exists
+                return res.status(400).json({ msg: 'User already exists' });
+            }
+
+            user = new User({
+                name, 
+                email, 
+                password
+            });
+            //Password Encryption
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            res.send('User Saved');
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
     }
 );
 
