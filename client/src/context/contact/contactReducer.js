2aa84d95e02b358635b000b0b5438518df78d291
Sanskar Bajpai <sanskarbajpai2907@gmail.com>
2020-12-20 20:05:58 +0530

    UpdateEditContacts: Added the Update & Edit action types.
    this commit introduces the Update and contact editing action,
    along with a couple formatting changes. The warning of exporting
    a const arrow function has also been fixed.
    A clear button has also been introduced beneath the 'Update
    Contact' button.

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -8,7 +8,7 @@ import {
   CLEAR_FILTER,
 } from "../types";
 
-export default (state, action) => {
+const Types = (state, action) => {
   switch (action.type) {
     case ADD_CONTACT:
       return {
@@ -22,17 +22,39 @@ export default (state, action) => {
           (contact) => contact.id !== action.payload
         ),
       };
+    case UPDATE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.map((contact) =>
+          contact.id === action.payload.id ? action.payload : contact
+        ),
+      };
     case SET_CURRENT:
       return {
         ...state,
-         current: action.payload
+        current: action.payload,
       };
-      case CLEAR_CURRENT:
+    case CLEAR_CURRENT:
       return {
         ...state,
-         current: null
+        current: null,
+      };
+    case FILTER_CONTACTS:
+      return {
+        ...state,
+        filtered: state.contacts.filter((contact) => {
+          const regex = new RegExp(`${action.payload}`, "gi"); //global case insensitive
+          return contact.name.match(regex) || contact.email.match(regex);
+        }),
+      };
+    case CLEAR_FILTER:
+      return {
+        ...state,
+        filtered: null,
       };
     default:
       return state;
   }
 };
+
+export default Types;
