480699084e3c5d924f0dca471a20815d6efb1e99
Sanskar Bajpai <sanskarbajpai2907@gmail.com>
2020-12-22 17:55:22 +0530

    AuthState: Removed typo in the from script.

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from "react";
 import AuthContext from "./authContext";
-import authReducer from "./authtReducer";
+import authReducer from "./authReducer";
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
