de9b340b8b2ae494a784bc94fbba847810f6fa6f
Sanskar Bajpai <sanskarbajpai2907@gmail.com>
2020-12-22 17:50:36 +0530

    Authentincation: Added the Auth Reducer, State and Context.
    this commit introduces the authentication feature in the app.
    It adds the auth context api and handles the declares the
    states for the auth types. This commit also formats the App.js
    script.

---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,52 @@
+import React, { useReducer } from "react";
+import AuthContext from "./authContext";
+import authReducer from "./authtReducer";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS,
+} from "../types";
+
+const AuthState = (props) => {
+  //temporary data, will be taken in from the backend later on.
+  const initialState = {
+    token: localStorage.getItem("token"),
+    isAuthenticatd: null,
+    loading: true,
+    user: null,
+    error: null,
+  };
+
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  //Load User
+
+  //Register User
+
+  //Login User
+
+  //Logout
+
+  //Clear Errors
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticatd: state.isAuthenticatd,
+        loading: state.loa,
+        user: state.user,
+        error: state.error,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthState;
