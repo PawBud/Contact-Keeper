20d69b9378ac834692262d8396fdbb03ff5dde1e
Sanskar Bajpai <sanskarbajpai2907@gmail.com>
2023-01-17 15:37:12 +0100

    fixed stuff to run the app

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI");
+const db = "mongodb+srv://softcont:softcont2022@softw-cont.634tgax.mongodb.net/?retryWrites=true&w=majority";
 
 const connectDB = async () => {
   try {
